5af9d74a4a2fc5272c786415f43c7be5fdf66e7a
Neel Shah <neelshah.sa@gmail.com>
2017-10-29 01:34:22 +0200

    tweak polybar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -36,7 +36,7 @@ myLayout = desktopLayoutModifiers $ smartBorders $ spacingWithEdge space $ layou
      -- The default number of windows in the master pane
      nmaster = 1
      -- Default proportion of screen occupied by master pane
-     ratio = 1/2
+     ratio = 2/3
      -- Percent of screen to increment by when resizing panes
      delta = 3/100
      -- spacing between windows
