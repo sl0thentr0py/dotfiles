d8296fcbc45c1510d3d4bed316bed88cf4840322
Neel Shah <neelshah.sa@gmail.com>
2017-11-15 10:58:02 +0100

    move to DP1

--- .config/monitor-hotplug.sh
+++ .config/monitor-hotplug.sh
@@ -5,14 +5,14 @@ export DISPLAY=:0
 export XAUTHORITY=/home/neel/.Xauthority
 
 function connect(){
-  xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 1280x0 --rotate normal --output LVDS1 --primary --mode 1280x800 --pos 0x0 --rotate normal --output VGA1 --off
+  xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --mode 2560x1440 --pos 1280x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1280x800 --pos 0x1360 --rotate normal --output VGA1 --off
 }
 
 function disconnect(){
   xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1280x800 --pos 0x0 --rotate normal --output VGA1 --off
 }
 
-xrandr | grep "HDMI1 connected" &> /dev/null && connect || disconnect
+xrandr | grep "DP1 connected" &> /dev/null && connect || disconnect
 
 # only relaunch bars if not init mode
 if ! [ "$INIT_MODE" = "1" ]; then
