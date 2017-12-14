c91f7b20fade4b1cfbeb1c2ff98f2055df33c8ab
Neel Shah <neelshah.sa@gmail.com>
2017-12-14 17:41:17 +0100

    DPI and urxvt url fix

--- .config/monitor-hotplug.sh
+++ .config/monitor-hotplug.sh
@@ -5,11 +5,11 @@ export DISPLAY=:0
 export XAUTHORITY=/home/neel/.Xauthority
 
 function connect(){
-  xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --mode 2560x1440 --pos 1280x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1280x800 --pos 0x1360 --rotate normal --output VGA1 --off
+  xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --mode 2560x1440 --pos 1280x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1280x800 --pos 0x1360 --dpi 113 --rotate normal --output VGA1 --off
 }
 
 function disconnect(){
-  xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1280x800 --pos 0x0 --rotate normal --output VGA1 --off
+  xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1280x800 --pos 0x0 --dpi 113 --rotate normal --output VGA1 --off
 }
 
 xrandr | grep "DP1 connected" &> /dev/null && connect || disconnect
