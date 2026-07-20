ef6f5197855c580b2a8770fa8b01bdedb7998154
Neel Shah <neelshah.sa@gmail.com>
2026-07-20 16:58:00 +0200

    fix ruby lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,6 +4,14 @@ return {
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPost", "BufNewFile" },
+    init = function()
+      -- mise's `latest` Ruby can be newer than the Ruby versions supported by
+      -- some of this repository's gemfiles. Keep the LSP runtime stable while
+      -- Ruby LSP still uses each project's own composed bundle.
+      vim.lsp.config("ruby_lsp", {
+        cmd = { "mise", "exec", "ruby@3.4", "--", "ruby-lsp" },
+      })
+    end,
     config = function()
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
